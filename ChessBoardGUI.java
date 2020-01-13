94786632dcc4f7e031c04e1378affeb3260d1bee
mcmg9 <51424788+mcmg9@users.noreply.github.com>
2020-01-13 17:00:42 -0500

    Add files via upload

---
+++ ChessBoardGUI.java
@@ -0,0 +1,237 @@
+/**
+ * @author Martin Guevara
+ * @version January 14th, 2019
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+
+public class ChessBoardGUI extends JFrame implements ActionListener{
+  
+  JButton[][] buttonArray = new JButton[8][8];
+  private int xCorChoice = 8;
+  private int yCorChoice = 8;
+  private int xCorMove = 8;
+  private int yCorMove = 8;
+  private int clickCounter = 0;
+  
+  JLabel player1Wins = new JLabel("Player 1 Wins!");
+  JLabel player2Wins = new JLabel("Player 2 Wins!");
+  
+  public ChessBoardGUI(){
+    super("Chess Game"); 
+    
+    setLayout(null);
+    
+    String highScoreMoves = "";
+    
+    try {
+      File highScore = new File("WinningScores.txt");
+      FileReader in = new FileReader(highScore);
+      BufferedReader br = new BufferedReader(in);
+      highScoreMoves = br.readLine();
+    }catch(FileNotFoundException e){
+      System.out.println(e);
+    } catch (IOException e2) {
+      System.out.println(e2);
+    }
+    
+    JLabel highScoreText = new JLabel("High Score: " + highScoreMoves);
+    highScoreText.setBounds(830, 50, 250, 100);
+    highScoreText.setLayout(new BorderLayout());
+    highScoreText.setFont(new Font("Arial Black", Font.PLAIN, 30));
+    add(highScoreText);
+    
+    player2Wins.setBounds(830, 150, 250, 200);
+    player2Wins.setLayout(new BorderLayout());
+    player2Wins.setFont(new Font("Arial Black", Font.PLAIN, 30));
+    add(player2Wins);
+    player2Wins.setVisible(false);
+    
+    player1Wins.setBounds(830, 150, 250, 200);
+    player1Wins.setLayout(new BorderLayout());
+    player1Wins.setFont(new Font("Arial Black", Font.PLAIN, 30));
+    add(player1Wins);
+    player1Wins.setVisible(false);
+    
+    for(int i = 0; i < 8; i++) {
+      for(int j = 0; j < 8; j++){
+        buttonArray[i][j] = new JButton("");
+        buttonArray[i][j].setFont(new Font("Arial", Font.PLAIN, 0));
+        buttonArray[i][j].addActionListener(this); 
+        if ((i + j) % 2 == 0){
+          buttonArray[i][j].setBackground(Color.WHITE);
+          buttonArray[i][j].setOpaque(true);
+        }else{
+          buttonArray[i][j].setBackground(Color.BLACK); 
+          buttonArray[i][j].setOpaque(true);
+        }
+        
+      }
+    }
+    
+    int posX = 0;
+    int posY = 0;
+    
+    for(int i = 0; i < 8; i++) {
+      for(int j = 0; j < 8; j++){
+        buttonArray[i][j].setBounds(posX, posY, 100, 100);
+        add(buttonArray[i][j]);
+        posX += 100;
+      }
+      posY += 100;
+      posX = 0;
+    }
+    
+    setResizable(false);
+    
+  }//chessBoardGUI
+  
+  /**
+   * gives a clone of a buttonArray
+   * @return clone of the JButton array buttonArray
+   */
+  public JButton[][] getButtonArray(){
+    return buttonArray.clone();
+  }//getButtonArray
+  
+  /**
+   * gives a button an icon of a piece
+   * @param JButton array to get buttons, int for x coordinate of button, int for y coordinate of button, char for the piece type, int for the team of the piece
+   */
+  public void giveIcon(JButton[][] c, int x, int y, char piece, int team){
+    
+    Icon blackPawn = new ImageIcon(getClass().getResource("/ChessPieces/PawnBlack.png"));
+    Icon blackTower = new ImageIcon(getClass().getResource("/ChessPieces/TowerBlack.png"));
+    Icon blackKnight = new ImageIcon(getClass().getResource("/ChessPieces/KnightBlack.png"));
+    Icon blackBishop = new ImageIcon(getClass().getResource("/ChessPieces/BishopBlack.png"));
+    Icon blackQueen = new ImageIcon(getClass().getResource("/ChessPieces/QueenBlack.png"));
+    Icon blackKing = new ImageIcon(getClass().getResource("/ChessPieces/KingBlack.png"));
+    Icon whitePawn = new ImageIcon(getClass().getResource("/ChessPieces/PawnWhite.png"));
+    Icon whiteTower = new ImageIcon(getClass().getResource("/ChessPieces/TowerWhite.png"));
+    Icon whiteKnight = new ImageIcon(getClass().getResource("/ChessPieces/KnightWhite.png"));
+    Icon whiteBishop = new ImageIcon(getClass().getResource("/ChessPieces/BishopWhite.png"));
+    Icon whiteQueen = new ImageIcon(getClass().getResource("/ChessPieces/QueenWhite.png"));
+    Icon whiteKing = new ImageIcon(getClass().getResource("/ChessPieces/KingWhite.png"));
+    
+    if (team == 1){ //team one is black
+      if (piece == 'p'){
+        c[x][y].setIcon(blackPawn);
+      }else if(piece == 't'){
+        c[x][y].setIcon(blackTower);
+      }else if(piece == 'k'){ //lowercase 'k' for knight
+        c[x][y].setIcon(blackKnight);
+      }else if(piece == 'b'){
+        c[x][y].setIcon(blackBishop);
+      }else if(piece == 'q'){
+        c[x][y].setIcon(blackQueen);
+      }else if(piece == 'K'){ //uppercase 'K' for king 
+        c[x][y].setIcon(blackKing);
+      }   
+    }else if(team == 2){ //team two is white
+      if (piece == 'p'){
+        c[x][y].setIcon(whitePawn);
+      }else if(piece == 't'){
+        c[x][y].setIcon(whiteTower);
+      }else if(piece == 'k'){ //lowercase 'k' for knight
+        c[x][y].setIcon(whiteKnight);
+      }else if(piece == 'b'){
+        c[x][y].setIcon(whiteBishop);
+      }else if(piece == 'q'){
+        c[x][y].setIcon(whiteQueen);
+      }else if(piece == 'K'){ //uppercase 'K' for king 
+        c[x][y].setIcon(whiteKing);
+      }
+    }
+  }//giveIcon
+  
+  /**
+   * removes the icon from a button
+   * @param JButton array to get buttons, int for x coordinate of button, int for y coordinate of button
+   */
+  public void removeIcon(JButton[][] c, int x, int y){
+    c[x][y].setIcon(null);
+  }//removeIcon
+  
+  /**
+   * gets the coordinates of the button pressed an distinguishes whether it is the original button or the move button
+   * @param ActionEvent the click of a button
+   */
+  public void actionPerformed(ActionEvent e){
+    for (int i = 0; i < 8; i++) {
+      for (int j = 0; j < 8; j++) {
+        if(buttonArray[i][j] == e.getSource()){ 
+          if (clickCounter % 2 == 0){
+            xCorChoice = i;
+            yCorChoice = j;
+            clickCounter += 1;
+          }else{
+            xCorMove = i;
+            yCorMove = j;
+            clickCounter += 1;
+          }
+        }
+      }
+    }
+  }//actionPerformed
+  
+  /**
+   * gives the x coordinate of the original click
+   * @return the x coordinate of the original click unless the click is for the move coordinates, in which case it will return 8
+   */
+  public int getButtonCoordChoiceX(){
+    if (clickCounter % 2 == 1)
+      return xCorChoice;
+    else
+      return 8;
+  }//getButtonCoordChoiceX
+  
+  /**
+   * gives the y coordinate of the original click
+   * @return the y coordinate of the original click unless the click is for the move coordinates, in which case it will return 8
+   */
+  public int getButtonCoordChoiceY(){
+    if (clickCounter % 2 == 1)
+      return yCorChoice;
+    else
+      return 8;
+  }//getButtonCoordChoiceY
+  
+  /**
+   * gives the x coordinate of the button to move to
+   * @return the x coordinate of the button to move to unless the click is for the original coordinates, in which case it will return 8
+   */
+  public int getButtonCoordMoveX(){
+    if (clickCounter % 2 == 0)
+      return xCorMove;
+    else
+      return 8;
+  }//getButtonCoordMoveX
+  
+  /**
+   * gives the y coordinate of the button to move to
+   * @return the y coordinate of the button to move to unless the click is for the original coordinates, in which case it will return 8
+   */
+  public int getButtonCoordMoveY(){
+    if (clickCounter % 2 == 0)
+      return yCorMove;
+    else
+      return 8;
+  }//getButtonCoordMoveY
+  
+  /**
+   * sets the winning text for player 1 to visible if they win
+   */
+  public void player1Winner(){
+    player1Wins.setVisible(true);
+  }//player1Winner
+  
+  /**
+   * sets the winning text for player 2 to visible if they win
+   */
+  public void player2Winner(){
+    player2Wins.setVisible(true);
+  }//player2Winner
+  
+}//chessBoardGUI
---
+++ ChessPieceInterface.java
@@ -0,0 +1,11 @@
+/**
+ * @author Martin Guevara
+ * @version January 14th, 2019
+ */
+public interface ChessPieceInterface{
+  
+  public int getTeam();
+  
+  public int getType();
+  
+}//ChessPieceInterface
